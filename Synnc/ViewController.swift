b1706be0265dd90946b9a14e8099d20092a35109
doguhanokumus <douhan@gmail.com>
2015-11-30 17:18:44 +0100

    Initial Commit

---
+++ Synnc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Synnc
+//
+//  Created by Arda Erzin on 11/30/15.
+//  Copyright © 2015 Arda Erzin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
